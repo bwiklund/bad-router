a8091430c3d44a4393a195a11b23fad313018b2c
Ben Wiklund <ben@daisyowl.com>
2014-03-16 13:19:25 -0700

    test cleanup

--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -2,56 +2,53 @@ require './lib/router'
 
 describe Router do
 
+  before :each do
+    @router = Router.new
+  end
+
   # helper to dry up these cases
-  def expect_request_status(router,path,verb,status)
-    router.call('PATH_INFO' => path,'REQUEST_METHOD' => verb.to_s)[0].should == status
+  def expect_request_status(path,verb,status)
+    @router.call('PATH_INFO' => path,'REQUEST_METHOD' => verb.to_s)[0].should == status
   end
 
   it "should exist" do
-    Router.new
+    @router.nil?.should == false
   end
 
-  it "should respond to calls from rack" do
-    response = Router.new.call '/'
-    response[0].should == 404
+  it "should respond 404 if no routes are set" do
+    expect_request_status '/', 'GET', 404
   end
 
   it "should be able to route based on regex" do
-    router = Router.new
-    router.get '/' do [200,{},["acceptable."]] end
-    response = router.call('PATH_INFO' => '/','REQUEST_METHOD' => 'GET')
-    response[0].should == 200
+    @router.get '/' do [200,{},["acceptable."]] end
+    expect_request_status '/', 'GET', 200
   end
 
   it "should be able to route paths other than /" do
-    router = Router.new
-    router.get "/foo" do [500,{},["bad."]] end
-    router.get "/bar" do [200,{},["good."]] end
-    router.get "/baz" do [500,{},["bad."]] end
-    expect_request_status router, '/foo', 'GET', 500
-    expect_request_status router, '/bar', 'GET', 200
-    expect_request_status router, '/baz', 'GET', 500
+    @router.get "/foo" do [500,{},["bad."]] end
+    @router.get "/bar" do [200,{},["good."]] end
+    @router.get "/baz" do [500,{},["bad."]] end
+    expect_request_status '/foo', 'GET', 500
+    expect_request_status '/bar', 'GET', 200
+    expect_request_status '/baz', 'GET', 500
   end
 
   it "should be able to route based on method" do
-    router = Router.new
-    router.get '/' do [500,{},["bad."]] end
-    router.post '/' do [200,{},["good!"]] end
-    expect_request_status router, '/', 'POST', 200
+    @router.get '/' do [500,{},["bad."]] end
+    @router.post '/' do [200,{},["good!"]] end
+    expect_request_status '/', 'POST', 200
   end
 
   it "can match all verbs" do
-    router = Router.new
-    router.all '/' do [200,{},[["ok"]]] end
-    expect_request_status router, '/', 'GET', 200
-    expect_request_status router, '/', 'POST', 200
+    @router.all '/' do [200,{},[["ok"]]] end
+    expect_request_status '/', 'GET', 200
+    expect_request_status '/', 'POST', 200
   end
 
   it "can match certain verbs" do
-    router = Router.new
-    router.methods [:GET,:POST], '/' do [200,{},[["ok"]]] end
-    expect_request_status router, '/', 'GET', 200
-    expect_request_status router, '/', 'POST', 200
-    expect_request_status router, '/', 'DELETE', 404
+    @router.methods [:GET,:POST], '/' do [200,{},[["ok"]]] end
+    expect_request_status '/', 'GET', 200
+    expect_request_status '/', 'POST', 200
+    expect_request_status '/', 'DELETE', 404
   end
 end
